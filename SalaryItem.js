b3ab5323f6c3d9d92ec8b10fc2fddfde4fd38b3a
Aliza Tariq <114760516+AlizayTariq@users.noreply.github.com>
2022-10-25 11:54:14 +0500

    Add files via upload

---
+++ SalaryItem.js
@@ -0,0 +1,82 @@
+var vm = new Vue({
+  el: "#vue_det",
+  data: {
+    firstname: "Dr. Muhammad",
+    lastname: "Nauman",
+    htmlcontent: "<div><h1>Vue Js Template</h1></div>",
+    object: {
+      Site: " Geeks for Geeks ",
+      Framework: " Vue ",
+      Type: " Article ",
+      Subject: " v-for on a <template> ",
+    },
+    myEmp: [
+      {
+      
+        ID: "101",
+        Name: "Uzair",
+        Scale: "4",
+        Salary: "20,000",
+        
+      },
+      {
+        ID: "102",
+        Name: "Umer",
+        Scale: "6",
+        Salary: "25,000",
+      },
+      {
+        ID: "103",
+        Name: "Abu Baker",
+        Scale: "8",
+        Salary: "30,000",
+      },
+      {
+        ID: "105",
+        Name: "Irum",
+        Scale: "10",
+        Salary: "35000",
+      },
+      {
+        ID: "106",
+        Name: "Kinza",
+        Scale: "12",
+        Salary: "40,000",
+      },
+
+      {
+        ID: "107",
+        Name: "14",
+
+        Scale: "14",
+        Salary: "45,000",
+      },
+      {
+        ID: "108",
+        Name: "Arslan",
+    
+
+        Scale: "16",
+        Salary: "50,000",
+      },
+       {
+        ID: "110",
+        Name: "Arslan",
+        Scale: "18",
+        Salary: "55,000",
+      },
+       {
+        ID: "111",
+        Name: "Akber",
+        Scale: "20",
+        Salary: "50,000",
+      },
+       {
+        ID: "112",
+        Name: "Rana Arslan",
+        Scale: "22",
+        Salary: "50,000",
+      },
+    ],
+  },
+});
